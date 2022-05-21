5b35b33a8f7ebaf2dd30f27021fa976f8ed0cb87
0304sk <saranya.kumar0304@gmail.com>
2022-05-21 13:42:00 +0530

    new code

--- src/Programs/test.java
+++ src/Programs/test.java
@@ -6,7 +6,9 @@ public class test {
 	
 	public static void main(String[] args) {
 		//Print odd numbers between 1 to 10
-		System.out.println("Prasanna changing code in same line");
+		System.out.println("Resolving the issue");
+		System.out.println("Resolving the issue");
+
 		for (int i = 1; i <=10; i++) {
 			
 			int a = i%2;
