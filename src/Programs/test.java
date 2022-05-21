989e95a23c6634d17bb0f3370b5b372146661273
0304sk <saranya.kumar0304@gmail.com>
2022-05-21 13:44:13 +0530

    new

--- src/Programs/test.java
+++ src/Programs/test.java
@@ -7,6 +7,8 @@ public class test {
 	public static void main(String[] args) {
 		//Print odd numbers between 1 to 10
 		System.out.println("adding a new line for git test");
+		System.out.println("adding a new line for git test");
+
 		
 		for (int i = 1; i <=10; i++) {
 			
